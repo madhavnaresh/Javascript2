46a2ac906bb8bb5189d42a626c65776f8f32c4bc
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-09 10:01:38 +0000

    conersion and comparison

---
+++ 1_basics/3_conversionOperation.js
@@ -0,0 +1,63 @@
+/*
+let score="33abc"
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+*/
+
+/*
+let score2=null
+console.log(typeof score);
+
+let valueInNumber2=Number(score2)
+console.log(typeof valueInNumber2);
+
+console.log(valueInNumber2);
+*/
+
+
+// for boolean
+/*
+1 => true
+0 => false
+"" => false
+"madhav" => true
+*/
+
+// ******************** Operations ********************
+ let value=3
+ let negValue=-value
+// console.log(negValue);
+
+/* 
+console.log(2+2);
+ console.log(2-2);
+ console.log(2/2);
+ console.log(2*2);
+ console.log(2**3); //power
+ console.log(2%2);
+ */
+
+ /*
+ let str1="madhav"
+ let str2=" gupta"
+ //console.log(str1+str2); // will add strings
+ */
+
+ /*
+ //According to Type-Conversion of JS 
+ console.log("1"+2); //12
+ console.log(1+"2"); //12
+ console.log("1"+2+2); //122
+ console.log(1+2+"2"); //32
+ */
+
+ /*
+ let gameCounter=100
+ ++gameCounter;
+ console.log(gameCounter);
+ */
+
---
+++ 1_basics/4_comparison.js
@@ -0,0 +1,14 @@
+//Logical Operators
+
+ /*
+ console.log(2==1);
+ console.log(2>1);
+ console.log(2<1);
+ console.log(2<=1);
+ console.log(2>=1);
+ */
+
+ //Strictly check (===)
+ console.log(2===2); //true
+ console.log("2"===2); //false bcz first it convert type of variables than check and here first '2' is string that's no comparison
+ 
