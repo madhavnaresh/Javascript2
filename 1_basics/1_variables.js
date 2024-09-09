153bca0a2870ae0591df36b02e32a532b3414bc4
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-09 06:48:03 +0000

    learning

---
+++ 1_basics/1_variables.js
@@ -0,0 +1,6 @@
+const Id=1369
+let Name="Madhav"
+var Email="mg123@gmail.com" //try not to use 'var' bcz of issue in block and functional scope
+let City
+
+console.table([Id, Name, Email, City])
---
+++ 1_basics/2_datatypes.js
@@ -0,0 +1,12 @@
+let name="mg"
+let age=20
+let loggedIn=true
+
+/*Number
+ BigInt
+ string
+ boolean
+ null
+ unefined
+ symbol
+ */
