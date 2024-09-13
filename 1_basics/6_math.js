c0294357b38fe5ee506d9ef8d8c7e048da509473
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-13 10:45:42 +0000

    math function

--- 1_basics/6_math.js
+++ 1_basics/6_math.js
@@ -9,11 +9,13 @@ console.log(Math.max(4,5,2,9,7));
 
 */
 
-
+/*
 console.log(Math.random()); //return a random number b/w 0 and 1
 console.log(Math.random()*10); // it can still return value 0 (zero)
 console.log((Math.random()*10)+1); //but it'll not return zero
+*/
 
+const min=10
+const max=20
 
-
-
+console.log(Math.floor(Math.random()*(max-min+1))+min); // floor value of [{random*(11)}+10)]
