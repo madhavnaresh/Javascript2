4dceb88aceafaee69dadf10b2c574bffe6d94364
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-13 09:13:33 +0000

    string and data type

---
+++ 1_basics/5_string.js
@@ -0,0 +1,18 @@
+let name ="madhav"
+let repocount=15
+//console.log(name + repocount);
+
+console.log(`my name is ${name} and my repocount is ${repocount}`);
+
+const gamename=new String('Madhavg')
+console.log(gamename[0]); //return value of index 0
+
+console.log(gamename.length); //length of string
+
+console.log(gamename.toUpperCase()); //change case of string to uppercase
+
+console.log(gamename.charAt(3)); // will help to find character of same index no.
+
+console.log(gamename.indexOf('v')); // return index no of character
+
+
