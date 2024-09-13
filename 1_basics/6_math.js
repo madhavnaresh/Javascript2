51861bb3e2a9188147e0fcf1f651af30ca46306d
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-13 09:43:13 +0000

    math function

---
+++ 1_basics/6_math.js
@@ -0,0 +1,19 @@
+/*
+console.log(Math);
+console.log(Math.abs(-4)); // convert negative into positive
+console.log(Math.round(4.6)); //round off the value
+console.log(Math.ceil(4.1)); // return ceil value
+console.log(Math.floor(4.9)); // return floor value
+console.log(Math.min(4,5,2,9,7));
+console.log(Math.max(4,5,2,9,7));
+
+*/
+
+
+console.log(Math.random()); //return a random number b/w 0 and 1
+console.log(Math.random()*10); // it can still return value 0 (zero)
+console.log((Math.random()*10)+1); //but it'll not return zero
+
+
+
+
