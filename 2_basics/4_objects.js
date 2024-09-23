233d165bbb1da1505d9236fb2e9125fcd1b12f73
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-23 08:37:25 +0000

    object

--- 2_basics/4_objects.js
+++ 2_basics/4_objects.js
@@ -28,5 +28,34 @@ const tinderUser={}
 
  // object=Object.assign(target,source)
  const ob4=Object.assign({},ob1,ob2) //all values will be assigned in the curly braces coz that is target
- console.log(ob4);
+ //console.log(ob4);
+
+ const ob5={...ob1,...ob2} //same as assign method
+ //console.log(ob5);
+ 
+ const users=[
+    {
+        id:231,
+        mail:"2@mail.com"
+    },
+    {
+        id:232,
+        mail:"4@mail.com"
+    },
+    {
+        id:233,
+        mail:"6@mail.com"
+    },
+ ]
+ //accessing from users
+ console.log(users[1].mail);
+
+ //using tinderUser object
+
+ console.log(Object.keys(tinderUser)); //accessing key from an object in form of array
+ console.log(Object.values(tinderUser)); //accessing key/value from an object in form of array
+ console.log(Object.entries(tinderUser)); //give all entries in form of array in array
+ 
+ 
+ 
  
