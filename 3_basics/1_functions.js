6e1cc4e3bc6731a793ff6553fc681241bc66b8ff
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-23 09:44:12 +0000

    function intro

---
+++ 3_basics/1_functions.js
@@ -0,0 +1,17 @@
+function myName(){
+    console.log("M");
+    console.log("A");
+    console.log("D");
+    console.log("H");
+    console.log("A");
+    console.log("V");
+}
+
+myName //for refence purpose
+myName() // function calling
+
+function ADDTWONUM(num1, num2){
+    console.log(num1+num2);
+}
+
+ADDTWONUM(4,6)
