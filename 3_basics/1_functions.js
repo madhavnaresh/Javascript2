49ac387177a031a529401264988cfdd548d16519
Madhav Gupta <madhavgupta639@gmail.com>
2024-09-23 10:04:30 +0000

    functions

--- 3_basics/1_functions.js
+++ 3_basics/1_functions.js
@@ -1,3 +1,5 @@
+
+/*
 function myName(){
     console.log("M");
     console.log("A");
@@ -10,8 +12,35 @@ function myName(){
 myName //for refence purpose
 myName() // function calling
 
-function ADDTWONUM(num1, num2){
+
+function addtwonum(num1, num2){
     console.log(num1+num2);
 }
 
-ADDTWONUM(4,6)
+addtwonum(4,6)
+
+function addnum(num1,num2){
+    return num1+num2
+}
+
+const result=addnum(3,7)
+console.log(result);
+*/
+
+
+function loggedIn(username){
+    return `${username} just logged in`
+}
+console.log(loggedIn("MG"));
+
+// extended version of code given above
+function logged(user){ //use when a chance that user will not enter a value
+    if(!user){
+        console.log("Please give an username");
+        return
+    }
+    else{
+        return `${user} logged in`
+    }
+}
+console.log(logged("ab"));
