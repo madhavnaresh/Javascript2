228176fc964a406ff1d3fb0a97d45bd78a61a247
Madhav Gupta <madhavgupta639@gmail.com>
2024-10-03 07:06:02 +0000

    function hoisting

--- 3_basics/2_scopes.js
+++ 3_basics/2_scopes.js
@@ -1,3 +1,5 @@
+
+/*
 //var c=300
 let a=100
 if(true){
@@ -10,3 +12,29 @@ if(true){
 console.log(a);
 // console.log(b);
 // console.log(c);
+*/
+
+
+/*
+function one(){
+    const username="mg"
+    function two(){
+        const website="yt"
+        console.log(username);
+    }
+    //console.log(website);
+    two()
+    
+}
+one()
+*/
+
+console.log(addone(5));
+function addone(num){
+    return num+1
+}
+
+const addtwo=function(num){
+    return num+2
+}
+console.log(addtwo(5))
