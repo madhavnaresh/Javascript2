98854ce16fa6d2d7683c427cfea4a4eede6255f3
Madhav Gupta <madhavgupta639@gmail.com>
2024-10-06 18:11:49 +0000

    normal function

---
+++ 3_basics/3_arrowfn.js
@@ -0,0 +1,13 @@
+const user={
+    username:"mg",
+    price:999,
+    welcomeMsg:function(){
+        console.log(`${this.username}, welcome to website`);
+        //console.log(this);
+        
+    }
+}
+//user.welcomeMsg()
+user.username="gupta"
+user.welcomeMsg()
+// console.log(this);
